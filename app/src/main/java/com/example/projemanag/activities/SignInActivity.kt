0172dd1f8cf7bd5b0d1d03988fc92564e31805e8
shubhamsharma7919 <shubhamsharma7919@gmail.com>
2021-12-10 16:38:01 +0530

    pepper1

---
+++ app/src/main/java/com/example/projemanag/activities/SignInActivity.kt
@@ -0,0 +1,35 @@
+package com.example.projemanag.activities
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.projemanag.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        setupActionBar()
+    }
+        private fun setupActionBar() {
+            setSupportActionBar(toolbar_sign_up_activity)
+            val actionBar = supportActionBar
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true)
+                actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+            }
+            toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
